086d883c2272a1ad25a1db2fcdfbc75f4b797c57
Valary Anguzuzu <anguzuzuvalary@gmail.com>
2025-02-13 12:56:24 +0300

    feat: responsiveness

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+// toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle('bx-x')
-  navbar.classList.toggle('active')
-
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
-
-
 
 // scroll sections active link
 
-let sections = document.querySelectorAll('section');
-let navlinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-  sections.forEach(sec => {
+  sections.forEach((sec) => {
     let top = window.scrollY;
     let offset = sec.offsetTop - 150;
     let height = sec.offsetHeight;
-    let id = sec.getAttribute('id');
+    let id = sec.getAttribute("id");
 
     if (top >= offset && top < offset + height) {
-      navlinks.forEach(links => {
-        links.classList.remove('active');
-        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
       });
-    };
+    }
   });
 
   // sticky navbar
-  let header = document.querySelector('header')
+  let header = document.querySelector("header");
 
-  header.classList.toggle('sticky', window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+  // remove toggle icon when click navbar link(scroll)
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
-
-
-
-
 //toggle dark mode
-const darkModeToggle = document.getElementById('dark-mode-toggle');
+const darkModeToggle = document.getElementById("dark-mode-toggle");
 
-darkModeToggle.addEventListener('click', () => {
-  document.body.classList.toggle('dark-mode');
+darkModeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
 });
-
